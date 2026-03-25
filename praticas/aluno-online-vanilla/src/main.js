e1b4f107d928fba46bd7f5d1fdaf7ae5c7fac9c2
Raphael <raphaelferraz5056@gmail.com>
2026-03-25 02:11:59 -0300

    feat: concluí a prática02. Close #3

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,36 @@
 import './style.css'
+
+function mudarCorMensagemErro(e){
+    e.style.color = '#ff0000';
+};
+
+const botao = document.querySelector("#botao-enviar-login");
+
+botao.addEventListener('click', () => {
+    const emailInput = document.querySelector("#e-mail");
+    const erroEmail = document.querySelector("#erro-email");
+    erroEmail.textContent = '';
+    
+    const senhaInput = document.querySelector("#senha");
+    const erroSenha = document.querySelector("#erro-senha")
+    erroSenha.textContent = '';
+    
+    if(emailInput.value == ''){
+        mudarCorMensagemErro(erroEmail);
+        erroEmail.textContent = 'O campo de e-mail é obrigatório.';
+        console.log(typeof emailInput.value);
+        return;
+    };
+
+    if(!emailInput.value.includes('@')){
+        mudarCorMensagemErro(erroEmail);
+        erroEmail.textContent = 'Digite um e-mail válido.'
+        return;
+    };
+
+    if(senhaInput.value == ''){
+        mudarCorMensagemErro(erroSenha);
+        erroSenha.textContent = 'O campo de senha é obrigatório.' 
+        return;
+    };
+});
